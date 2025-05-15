626c8848eb66a7c8fc2e7947d3b3f47c6d216709
Vitali Pinchuk <vit.pinchuck@gmail.com>
2025-05-15 17:43:15 +0300

    upd CI

--- src/components/LinksPanel/components/LinksLayout/LinksLayout.styles.ts
+++ src/components/LinksPanel/components/LinksLayout/LinksLayout.styles.ts
@@ -1,10 +1,9 @@
 import { css } from '@emotion/css';
-import { GrafanaTheme2 } from '@grafana/data';
 
 /**
  * Styles
  */
-export const getStyles = (theme: GrafanaTheme2) => {
+export const getStyles = () => {
   return {
     linksContainer: css`
       display: flex;
